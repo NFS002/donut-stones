283ba9941fbaa0dfda965ca66118aa0cef7e308e
noah <n.fs@hotmail.co.uk>
2021-10-11 22:20:36 +0100

    Updated script to include required module as local var

--- pre-commit.gh-pages.js
+++ pre-commit.gh-pages.js
@@ -1,7 +1,6 @@
-const fs = require("fs");
-
 /* Find and replace asset paths in html */
 async function beforeAdd() {
+  const fs = require("fs");
   const fileName = "dist/index.html";
 
   fs.readFile(fileName, "utf8", function (err, text) {
@@ -9,8 +8,8 @@ async function beforeAdd() {
       return console.log(err);
     }
 
-    var result = text.replaceAll("=/assets/", "=/donut-stones/assets/");
-    fs.writeFile(fileName, text, "utf8", function (err) {
+    var result = text.replaceAll("/assets/", "/donut-stones/assets/");
+    fs.writeFile(fileName, result, "utf8", function (err) {
       if (err) return console.log(err);
     });
   });
